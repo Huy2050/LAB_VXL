20f22a6c4ee0206d305138319c95942773c21336
DESKTOP-VN3GDTJ\Admin <tranhoangbahuy35@gmail.com>
2025-09-30 19:38:54 +0700

    INIT EX1 LAB2

---
+++ LAB2/Code/Core/Inc/display7SEG.h
@@ -0,0 +1,14 @@
+/*
+ * display7SEG.h
+ *
+ *  Created on: Sep 30, 2025
+ *      Author: Admin
+ */
+
+#ifndef INC_DISPLAY7SEG_H_
+#define INC_DISPLAY7SEG_H_
+
+void display7SEG(int num);
+void display7SEGWithEn();
+
+#endif /* INC_DISPLAY7SEG_H_ */
---
+++ LAB2/Code/Core/Src/display7SEG.c
@@ -0,0 +1,51 @@
+/*
+ * display7SEG.c
+ *
+ *  Created on: Sep 30, 2025
+ *      Author: Admin
+ */
+#include "display7SEG.h"
+#include "main.h"
+const uint8_t segmentMap[10] = {
+    0x3F, // 0 -> abcdef
+    0x06, // 1 -> bc
+    0x5B, // 2 -> abdeg
+    0x4F, // 3 -> abcdg
+    0x66, // 4 -> bcfg
+    0x6D, // 5 -> acdfg
+    0x7D, // 6 -> acdefg
+    0x07, // 7 -> abc
+    0x7F, // 8 -> abcdefg
+    0x6F  // 9 -> abcdfg
+};
+void display7SEG(int num){
+	if (num < 0 || num > 9){
+		return;
+	}
+	uint8_t mask = segmentMap[num];
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, (mask & 0x01) ? GPIO_PIN_RESET : GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, (mask & 0x02) ? GPIO_PIN_RESET : GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2, (mask & 0x04) ? GPIO_PIN_RESET : GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3, (mask & 0x08) ? GPIO_PIN_RESET : GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, (mask & 0x10) ? GPIO_PIN_RESET : GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, (mask & 0x20) ? GPIO_PIN_RESET : GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, (mask & 0x40) ? GPIO_PIN_RESET : GPIO_PIN_SET);
+}
+int state = 0;
+void display7SEGWithEn(){
+	switch (state){
+		case 0:
+			HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, GPIO_PIN_SET);
+			display7SEG(1);
+			state = 1;
+			break;
+		case 1:
+			HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, GPIO_PIN_RESET);
+			display7SEG(2);
+			state = 0;
+			break;
+	}
+}
+
